c20f9b5fc5d79fc0d7ab0b942d389180659d0fb3
Krzychu <stawicki2@gmail.com>
2018-04-08 23:07:58 +0200

    configured cors to allow all

--- CarDealershipWebApiCore/CarDealershipWebApiCore/Startup.cs
+++ CarDealershipWebApiCore/CarDealershipWebApiCore/Startup.cs
@@ -26,16 +26,27 @@ namespace CarDealershipWebApiCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+
             services.AddScoped<ICarDealershipRepository, CarDealershipRepository>();
 
             services.AddDbContext<CarDealershipDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CarDealership")));
         
             services.AddMvc();
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("MyPolicy");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
